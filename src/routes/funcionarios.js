8ab864684f0f7396a408777a079b4d1b529b9b9a
Clayton Moura <clee.gomes@gmail.com>
2021-11-22 09:36:35 -0300

    project push

---
+++ src/routes/funcionarios.js
@@ -0,0 +1,117 @@
+// API REST
+import express from 'express'
+import { connectToDatabase } from '../utils/mongodb.js'
+import { check, validationResult } from 'express-validator'
+
+const router = express.Router()
+const nomeCollection = 'cadFuncionario'
+const { db, ObjectId } = await connectToDatabase()
+
+/**********************************************
+ * Validações
+ * 
+ **********************************************/
+const validaColaborador = [  
+  check('nome', 'Nome é obrigatório').not().isEmpty(),
+  check('cargo', 'O cargo é obrigatório').not().isEmpty(),
+  check('telefone', 'O telefone é obrigatório').not().isEmpty(),
+  check('cpf', 'O CPF é obrigatório').not().isEmpty(),
+  check('inscricao', 'A inscrição é obrigatória').not().isEmpty(),  
+]
+
+
+/**********************************************
+ * GET /colaboradores/
+ * Lista todos
+ **********************************************/
+router.get("/", async (req, res) => {
+  try {
+    db.collection(nomeCollection).find({}).toArray((err, docs) => {
+      if (err) {
+        res.status(400).json(err) //bad request
+      } else {
+        res.status(200).json(docs) //retorna os documentos
+      }
+    })
+  } catch (err) {
+    res.status(500).json({ "error": err.message })
+  }
+})
+
+/**********************************************
+ * GET /colaboradores/:id
+ * Lista por id
+ **********************************************/
+router.get("/:id", async (req, res) => {
+  try {
+    db.collection(nomeCollection).find({ "_id": { $eq: ObjectId(req.params.id) } }).toArray((err, docs) => {
+      if (err) {
+        res.status(400).json(err) //bad request
+      } else {
+        res.status(200).json(docs) //retorna o documento
+      }
+    })
+  } catch (err) {
+    res.status(500).json({ "error": err.message })
+  }
+}) 
+
+/**********************************************
+ * POST /colaboradores/
+ * Cadastrar colaborador
+ **********************************************/
+router.post('/', validaColaborador, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(({
+      errors: errors.array()
+    }))
+  } else {
+    await db.collection(nomeCollection)
+      .insertOne(req.body)
+      .then(result => res.status(201).send(result)) //retorna o ID do documento inserido)
+      .catch(err => res.status(400).json(err))
+  }
+})
+
+/**********************************************
+ * PUT /colaboradores/
+ * Alterar por ID
+ **********************************************/
+router.put('/', validaColaborador, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(({
+      errors: errors.array()
+    }))
+  } else {
+    const colaboradorInput = req.body
+    await db.collection(nomeCollection)
+      .updateOne({ "_id": { $eq: ObjectId(req.body._id) } }, {
+        $set:
+        {
+          nome: colaboradorInput.nome,
+          cargo: colaboradorInput.cargo,
+          telefone: colaboradorInput.telefone,
+          cpf: colaboradorInput.cpf,
+          inscricao: colaboradorInput.inscricao
+        }
+      },
+        { returnNewDocument: true })
+      .then(result => res.status(202).send(result))
+      .catch(err => res.status(400).json(err))
+  }
+})
+
+/**********************************************
+ * DELETE /colaboradores/
+ * Apagar por id
+ **********************************************/
+router.delete('/:id', async (req, res) => {
+  await db.collection(nomeCollection)
+    .deleteOne({ "_id": { $eq: ObjectId(req.params.id) } })
+    .then(result => res.status(202).send(result))
+    .catch(err => res.status(400).json(err))
+})
+
+export default router
